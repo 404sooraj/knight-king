da0713751d10aa817bf970ddfb3264e58556cbde
404sooraj <soorajnambiar8@gmail.com>
2026-03-15 23:06:28 +0530

    chore: changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,36 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _ready():
-	$AnimatedSprite2D.play("idle")
+	animated_sprite.play("idle")
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+
+	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# movement
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+
+	if direction != 0:
 		velocity.x = direction * SPEED
+		
+		# flip sprite
+		if direction > 0:
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
---
+++ scripts/slime.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+const SPEED = 60
+var direction = 1
+
+# Wall collision
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+		
+	elif ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+		
+	position.x += direction * SPEED * delta
